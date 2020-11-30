4c16d42de683e61b4f475e0267c4323bbf41ddce
Guido <guidotangorra@gmail.com>
2020-11-30 10:33:03 +0100

    Aggiunto Sqllite

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        // Actions go here
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+            return View(model);
+        }
     }
 }
--- AspNetCoreTodo/Startup.cs
+++ AspNetCoreTodo/Startup.cs
@@ -12,6 +12,7 @@ using AspNetCoreTodo.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo
 {
@@ -35,6 +36,8 @@ namespace AspNetCoreTodo
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            //services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+            services.AddScoped<ITodoItemService, TodoItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
